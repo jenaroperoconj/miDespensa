ed6797571af2ac639cf341c51e70bfc49b5ec438
Jenaro M. <jenaroma1@gmail.com>
2024-10-12 01:36:27 -0300

    Primer commit inicial

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+
+import { routes } from './app/app.routes';
+import { AppComponent } from './app/app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideIonicAngular(),
+    provideRouter(routes, withPreloading(PreloadAllModules)), provideFirebaseApp(() => initializeApp({"projectId":"midespensa-2bd20","appId":"1:195777376620:web:596886585ee1e6d4b28349","storageBucket":"midespensa-2bd20.appspot.com","apiKey":"AIzaSyD_Ky3WNi36zpfmvpMc6H73KInOANQ_5UM","authDomain":"midespensa-2bd20.firebaseapp.com","messagingSenderId":"195777376620","measurementId":"G-4FSV510ZT8"})), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideFirestore(() => getFirestore()), provideStorage(() => getStorage()),
+  ],
+});
