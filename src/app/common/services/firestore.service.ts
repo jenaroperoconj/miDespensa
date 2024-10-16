1e28214bc3f262a42d6a1a262c829d42f0efcce2
Jenaro M. <jenaroma1@gmail.com>
2024-10-16 15:26:56 -0300

    Proyecto base con firebase

---
+++ src/app/common/services/firestore.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, inject } from '@angular/core';
+import { collection, collectionData, Firestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirestoreService {
+
+  private firestore: Firestore = inject(Firestore);
+  constructor() { }
+
+  getCollectionChanges<tipo>(path: string) {
+    const itemCollection = collection(this.firestore, path);
+    return collectionData(itemCollection) as Observable<tipo[]>;
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
@@ -9,11 +10,21 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)), provideFirebaseApp(() => initializeApp({"projectId":"midespensa-2bd20","appId":"1:195777376620:web:596886585ee1e6d4b28349","storageBucket":"midespensa-2bd20.appspot.com","apiKey":"AIzaSyD_Ky3WNi36zpfmvpMc6H73KInOANQ_5UM","authDomain":"midespensa-2bd20.firebaseapp.com","messagingSenderId":"195777376620","measurementId":"G-4FSV510ZT8"})), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideFirestore(() => getFirestore()), provideStorage(() => getStorage()),
+    provideRouter(routes, withPreloading(PreloadAllModules)), 
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()), 
+    provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, 
+    provideFirestore(() => getFirestore()), 
+    provideStorage(() => getStorage()),
   ],
 });
